90c0dba96819b29a47101e9e291724bf047ebe0a
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2024-02-03 18:37:25 +0700

    Эксперимент с "водяными знаками" чтобы не путаться с площадками.

--- DictionaryManagement_Common/StringExtension.cs
+++ DictionaryManagement_Common/StringExtension.cs
@@ -1,10 +1,9 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 public static class StringExtension
 {
     public static string HideSlash(this string value)
-    {        
+    {
         return value.Replace("\\", "__123455432__");
     }
 
@@ -17,5 +16,16 @@ public static class StringExtension
     {
         return Regex.Replace(value, @"\p{C}+", string.Empty);
     }
+    public static int Count(this string input, string substr)
+    {
+        int freq = 0;
 
+        int index = input.IndexOf(substr);
+        while (index >= 0)
+        {
+            index = input.IndexOf(substr, index + substr.Length);
+            freq++;
+        }
+        return freq;
+    }
 }
